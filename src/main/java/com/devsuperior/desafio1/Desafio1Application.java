dcd17f6c3ba29fd9cbb11ede8cad322bfc903f66
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2025-03-19 10:22:31 -0300

    Cifrao

--- src/main/java/com/devsuperior/desafio1/Desafio1Application.java
+++ src/main/java/com/devsuperior/desafio1/Desafio1Application.java
@@ -26,19 +26,19 @@ public class Desafio1Application  implements CommandLineRunner{
 
 	        Order order1  = new Order(1034, 150.0, 20.0);   
 	        System.out.println("Pedido código: " + order1.getCode());
-	        System.out.println("Valor Total: " + orderService.total(order1));
+	        System.out.println("Valor Total: R$ " + orderService.total(order1));
 	        
 	        System.out.println("__________________________________");	        
 	        
 	        Order order2  = new Order(2282, 800.0, 10.0);   
 	        System.out.println("Pedido código: " + order2.getCode());
-	        System.out.println("Valor Total: " + orderService.total(order2));
+	        System.out.println("Valor Total: R$ " + orderService.total(order2));
 	        
 	        System.out.println("__________________________________");	
 	        
 	        Order order3  = new Order(1309, 95.90, 0.0);   
 	        System.out.println("Pedido código: " + order3.getCode());
-	        System.out.println("Valor Total: " + orderService.total(order3));
+	        System.out.println("Valor Total: R$ " + orderService.total(order3));
 	
 
 	}
